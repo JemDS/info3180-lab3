6b99ebb685bb465898d8f9bdcb79b926c6e5f9cd
Deidre-Ann <d_jemison@hotmail.com>
2021-02-21 13:14:27 -0500

    Updated __init__.py file and Added Mailtrap test phtos to static folder

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,11 @@ from flask_mail import Mail
 from .config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['MAIL_SERVER']="smtp.mailtrap.io"
+app.config['MAIL_PORT']=465
+app.config['MAIL_USERNAME']="4e52affca2e588"
+app.config['MAIL_PASSWORD']="9ad110b78cfda0"
+app.config['SECRET_KEY']="dsj200695"
 
 mail = Mail(app)
 from app import views
